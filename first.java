62a806488da8d04ddf1cc34e41b08785c6db965f
darshitjoshi <darshit.0506@gmail.com>
2023-02-25 12:01:42 +0530

    trying something new

--- first.java
+++ first.java
@@ -4,6 +4,7 @@ class first
     public static void main(String[] args) {
         System.out.println("0"+"hello WOrld");        
 	System.out.println("i am darshit joshi");
+	System.out.println("I am doing this all using vim");
 
     }
 }
